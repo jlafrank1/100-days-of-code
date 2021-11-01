f902a0361d3c18948300bb40f72d1c881a8dc471
Jess LaFrank <jesslafrank@gmail.com>
2021-11-01 12:36:35 -0700

    Create and Show routes work

--- 01-10/day_01/tracker-be/controllers/projects.js
+++ 01-10/day_01/tracker-be/controllers/projects.js
@@ -13,8 +13,24 @@ router.get("/", async (req, res) => {
 });
 
 // -- show --
+router.get("/:id", async (req, res) => {
+  try {
+    const oneProject = await Projects.findById(req.params.id);
+    res.status(200).json(oneProject);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
 
 // -- create --
+router.post("/", async (req, res) => {
+  try {
+    const newProject = await Projects.create(req.body);
+    res.status(200).json(newProject);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
 
 // -- destroy --
 
