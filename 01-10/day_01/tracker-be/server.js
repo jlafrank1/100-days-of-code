609a2bc34337156244070a0a34a0bdb960ab0bdd
Jess LaFrank <jesslafrank@gmail.com>
2021-11-01 14:47:20 -0700

    Added CORS

--- 01-10/day_01/tracker-be/server.js
+++ 01-10/day_01/tracker-be/server.js
@@ -2,14 +2,28 @@ const express = require("express");
 const app = express();
 const PORT = 9000;
 require("./db/db");
+const cors = require("cors");
 
 // -- controllers --
 const projectsController = require("./controllers/projects");
 
 // -- cors --
+const whitelist = ["http://localhost:3000"];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("not allowed by CORS"));
+    }
+  },
+};
 
 // -- middleware --
 app.use(express.json());
+app.use(cors(corsOptions));
+
 app.use("/projects", projectsController);
 
 app.listen(PORT, () => {
