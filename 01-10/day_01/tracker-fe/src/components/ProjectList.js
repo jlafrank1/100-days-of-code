70d2415d85177fa4bc98b44507ce02bb08d5af18
Jess LaFrank <jesslafrank@gmail.com>
2021-11-01 14:44:56 -0700

    getProjects function set up, CORS error

--- 01-10/day_01/tracker-fe/src/components/ProjectList.js
+++ 01-10/day_01/tracker-fe/src/components/ProjectList.js
@@ -1,6 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const ProjectList = () => {
+  const [projects, setProjects] = useState([]);
+
+  const getProjects = async () => {
+    try {
+      const projects = await fetch("http://localhost:9000/projects");
+      const parsedProjects = await projects.json();
+      setProjects(parsedProjects);
+      console.log(parsedProjects);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getProjects();
+  }, []);
+
   return (
     <header>
       <h2>list!</h2>
