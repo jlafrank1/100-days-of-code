abc218ea202d7287b533b669ba2d9057a2527f0f
Jess LaFrank <jesslafrank@gmail.com>
2021-11-01 18:16:08 -0700

    Prepped for deployment

--- 01-10/day_01/tracker-be/server.js
+++ 01-10/day_01/tracker-be/server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
-const PORT = 9000;
 require("./db/db");
+const PORT = process.env.PORT || 9000;
+require("dotenv").config();
 const cors = require("cors");
 
 // -- controllers --
